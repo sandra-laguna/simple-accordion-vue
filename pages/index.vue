b5643c162a2c2a50846c66c98a95cf7a0c418913
sandra.theseedsquad <sandra@theseedsquad.com>
2021-08-02 19:49:28 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="font-body font-black text-3xl text-black">Home</h1>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Tecnología, diseño y marketing | The Seed Squad",
+      link: [{ rel: "canonical", hid: "canonical", href: `${this.$config.baseUrl}/` }]
+    };
+  }
+};
+</script>
