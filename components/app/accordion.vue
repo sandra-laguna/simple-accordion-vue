885819c7ad31a60fe07c9f516aa2b8ef565c3400
sandra.theseedsquad <sandra@theseedsquad.com>
2021-08-02 20:07:06 +0200

    edit:styles

---
+++ components/app/accordion.vue
@@ -0,0 +1,48 @@
+<template>
+  <section id="faq" class="relative w-full max-w-screen-md mx-auto py-12 px-4 sm:py-24">
+    <h1 class="mb-6 text-center text-3xl font-extrabold text-gray-900 sm:text-4xl"> Frequently asked questions </h1>
+
+    <div id="accordion" class="accordions">
+      <dl class="border-t-2 border-black-900 py-6" v-for="(row, id) in rows" :key="id">
+        <dt class="py-1 w-full text-black font-black flex justify-between" v-on:click="row.open = !row.open">
+          {{ row.term }}
+          <button @click="toggle" class="px-2">
+            <icon-arrowdown :class="[row.open ? 'hidden' : 'block', 'h-4 w-4']" />
+            <icon-arrowup :class="[row.open ? 'block' : 'hidden', 'h-4 w-4']" />
+          </button>
+        </dt>
+        <dd class="mb-2 text-gray-500" v-if="row.open">{{ row.details }}</dd>
+      </dl>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isOpen: false,
+    rows: {
+      faq1: {
+        term: "Lorem ipsum dolor sit amet.",
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nemo!",
+        open: false
+      },
+      faq2: {
+        term: "Lorem ipsum dolor sit amet.",
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nemo!",
+        open: false
+      },
+      faq3: {
+        term: "Lorem ipsum dolor sit amet.",
+        details: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, nemo!",
+        open: false
+      }
+    }
+  }),
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <h1 class="font-body font-black text-3xl text-black">Home</h1>
+  <div>
+    <h1
+      class="
+        max-w-3xl
+        mx-auto
+        text-center
+        font-body font-black
+        text-3xl text-blue-900
+        border-b-2 border-blue-900
+        pb-2
+        sm:text-4xl
+      "
+    >
+      Simple accordion with Vue &amp; TailwindCSS
+    </h1>
+    <app-accordion />
+  </div>
 </template>
-
-<script>
-export default {
-  head() {
-    return {
-      title: "Tecnología, diseño y marketing | The Seed Squad",
-      link: [{ rel: "canonical", hid: "canonical", href: `${this.$config.baseUrl}/` }]
-    };
-  }
-};
-</script>
